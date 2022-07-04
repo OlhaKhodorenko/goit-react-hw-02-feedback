58f518e0a516bbd7392890de0bab87adf5967d81
OlhaKhodorenko <khodorenkoo@gmail.com>
2022-07-04 19:27:24 +0300

    fixed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Section from './section/section';
 import FeedbackOptions from './feedbackOptions/feedbackOptions';
 import Statistics from './statistics/statistics';
 import Notification from './notification/notification';
+
 export default class App extends Component {
   state = {
     good: 0,
@@ -14,37 +15,40 @@ export default class App extends Component {
       [option]: prevState[option] + 1,
     }));
   };
-  handleAddGood = () => {
-    this.setState(prevState => ({
-      good: prevState.good + 1,
-    }));
-  };
-  handleAddNeutral = () => {
-    this.setState(prevState => ({
-      neutral: prevState.neutral + 1,
-    }));
-  };
-  handleAddBad = () => {
-    this.setState(prevState => ({
-      bad: prevState.bad + 1,
-    }));
-  };
+  // handleAddGood = () => {
+  //   this.setState(prevState => ({
+  //     good: prevState.good + 1,
+  //   }));
+  // };
+  // handleAddNeutral = () => {
+  //   this.setState(prevState => ({
+  //     neutral: prevState.neutral + 1,
+  //   }));
+  // };
+  // handleAddBad = () => {
+  //   this.setState(prevState => ({
+  //     bad: prevState.bad + 1,
+  //   }));
+  // };
   countTotalFeedback = () => {
     return Object.values(this.state).reduce((total, item) => total + item);
   };
   countPositiveFeedbackPercentage = () => {
     return Math.floor((this.state.good / this.countTotalFeedback()) * 100);
   };
-  showStatistics = () => {};
+  //showStatistics = () => {};
+  option = Object.keys(this.state);
+
   render() {
     return (
       <div>
         <Section title="Please leave feedback">
-          <FeedbackOptions
-            onGoodBtnClick={this.handleAddGood}
-            onNeutralBtnClick={this.handleAddNeutral}
-            onBadBtnClick={this.handleAddBad}
-          />
+          <div className="btnsWrap">
+            <FeedbackOptions
+              options={this.option}
+              onLeaveFeedback={this.handleUpdate}
+            />
+          </div>
         </Section>
         {this.countTotalFeedback() > 0 ? (
           <section title="Statistics">
